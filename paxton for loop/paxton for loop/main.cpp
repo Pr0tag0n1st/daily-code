009c0f2cadba8d22fc01d4ecec6539b58d80553d
Game110AM <Game110AM@605-110-STU12>
2017-02-16 08:13:10 -0700

    erere

---
+++ paxton for loop/paxton for loop/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (int i = 10; i < 100; i += 10) {
+		cout << i << endl;
+	}
+	for (int l = 20; l < 65; l +=3) {
+		cout << l << endl;
+	}
+	for (int j = 35; j < 90; j += 12) {
+		cout << j << endl;
+	}
+}
